719bfdbf3ce06b2be198b39466ad584582a3855a
Bryan Jazo <bryanjazo88@gmail.com>
2021-03-30 13:48:34 -0700

    configured heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require 'resolv-replace'
 require 'httparty'
-response = HTTParty.get("https://wger.de/api/v2/exercise/?format=json&language=2&limit=20&offset=60")
+response = HTTParty.get("https://wger.de/api/v2/exercise/?format=json&language=2&limit=20")
 new_response = response.parsed_response
 
 
